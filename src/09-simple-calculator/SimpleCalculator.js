0352120eede8f3e65eac83e16453911fdca33276
jessiezhao <jessiezhao1816@gmail.com>
2023-05-11 12:29:59 +1000

    branch09-change content

--- src/09-simple-calculator/SimpleCalculator.js
+++ src/09-simple-calculator/SimpleCalculator.js
@@ -1,33 +1,61 @@
-const initialState = {}
+import { useReducer } from "react"
 
-function reducer (state, action) {}
+const initialState = {
+  num1 : 0,
+  num2 : 0,
+  result : 0
+}
+
+function reducer (state, action) {
+  switch (action.type) {
+    case 'setNumber1':
+      return { ...state, num1:action.number}
+    case 'setNumber2':
+      return { ...state, num2:action.number}
+    case 'PLUS':
+      return { ...state, result: state.num1 + state.num2}
+    case 'MINUS':
+      return { ...state, result: state.num1 - state.num2}
+      case 'MULTIPLICATION':
+        return { ...state, result: state.num1 * state.num2}
+      case 'DIVISION':
+        return { ...state, result: state.num1 / state.num2}
+    case 'C':
+      return initialState
+  }
+}
 
 export default function SimpleCalculator () {
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const [state, dispatch] = useReducer(reducer, initialState)
+console.log(state)
+
 
   return (
     <div>
       <div>
         <h2>Number 1</h2>
         {numbers.map(number => (
-          <button key={number}>
+          <button key={number} onClick={() => dispatch({type: 'setNumber1', number})}>
             {number}
           </button>))}
       </div>
       <div>
         <h2>Number 2</h2>
         {numbers.map(number => (
-          <button key={number}>
+          <button key={number} onClick={() => dispatch({type: 'setNumber2', number})}>
             {number}
           </button>))}
       </div>
       <div>
         <h2>Actions</h2>
-        <button>+</button>
-        <button>-</button>
-        <button>c</button>
+        <button onClick = {() => dispatch({type: 'PLUS'})}>+</button>
+        <button onClick = {() => dispatch({type: 'MINUS'})}>-</button>
+        <button onClick = {() => dispatch({type: 'MULTIPLICATION'})}>x</button>
+        <button onClick = {() => dispatch({type: 'DIVISION'})}>/</button>
+        <button onClick = {() => dispatch({type: 'C'})}>c</button>
       </div>
-      <div><h2>Result:</h2></div>
+      <div><h2>Result:{state.result}</h2></div>
     </div>
   )
 }
